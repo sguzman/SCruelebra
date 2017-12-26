2eae619f04e74db64019cf9704b239c059fd05d8
Salvador Guzman <guzmansalv@gmail.com>
2017-12-26 12:52:27 -0800

    simple red square

--- src/main/scala/com/github/sguzman/scala/sculebra/Main.scala
+++ src/main/scala/com/github/sguzman/scala/sculebra/Main.scala
@@ -1,10 +1,8 @@
 package com.github.sguzman.scala.sculebra
 
 import org.scalajs.dom
-import org.scalajs.dom.{document, html}
+import org.scalajs.dom.document
 import org.scalajs.dom.html.Canvas
-
-import scala.util.Random
 
 object Main {
   case class Point(x: Int, y: Int){
@@ -14,36 +12,10 @@ object Main {
 
   def main(args: Array[String]): Unit = {
     val canvas = document.getElementById("canvas").asInstanceOf[Canvas]
-    init(canvas)
-  }
-
-  def init(canvas: html.Canvas): Unit = {
     val ctx = canvas.getContext("2d")
       .asInstanceOf[dom.CanvasRenderingContext2D]
 
-    var count = 0
-    var p = Point(0, 0)
-    val corners = Seq(Point(255, 255), Point(0, 255), Point(128, 0))
-
-    def clear(): Unit = {
-      ctx.fillStyle = "black"
-      ctx.fillRect(0, 0, 255, 255)
-    }
-
-    def run(): Unit = for (i <- 0 until 10){
-      if (count % 3000 == 0) clear()
-      count += 1
-      p = (p + corners(Random.nextInt(3))) / 2
-
-      val height = 512.0 / (255 + p.y)
-      val r = (p.x * height).toInt
-      val g = ((255-p.x) * height).toInt
-      val b = p.y
-      ctx.fillStyle = s"rgb($g, $r, $b)"
-
-      ctx.fillRect(p.x, p.y, 1, 1)
-    }
-
-    dom.window.setInterval(() => run(), 50)
+    ctx.fillStyle = "red"
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
   }
 }
