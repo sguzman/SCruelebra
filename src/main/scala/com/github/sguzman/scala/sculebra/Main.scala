6a595386426f2f03ca55c2f137e3bf03d5f28b2c
Salvador Guzman <guzmansalv@gmail.com>
2017-12-26 13:26:06 -0800

    set height and width of canvas to window size

--- src/main/scala/com/github/sguzman/scala/sculebra/Main.scala
+++ src/main/scala/com/github/sguzman/scala/sculebra/Main.scala
@@ -3,19 +3,26 @@ package com.github.sguzman.scala.sculebra
 import org.scalajs.dom
 import org.scalajs.dom.document
 import org.scalajs.dom.html.Canvas
+import org.scalajs.dom.raw.KeyboardEvent
 
 object Main {
-  case class Point(x: Int, y: Int){
-    def +(p: Point) = Point(x + p.x, y + p.y)
-    def /(d: Int) = Point(x / d, y / d)
-  }
-
   def main(args: Array[String]): Unit = {
     val canvas = document.getElementById("canvas").asInstanceOf[Canvas]
     val ctx = canvas.getContext("2d")
       .asInstanceOf[dom.CanvasRenderingContext2D]
 
-    ctx.fillStyle = "red"
+    canvas.width = dom.window.innerWidth.toInt
+    canvas.height = dom.window.innerHeight.toInt
+
+    ctx.fillStyle = "black"
     ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+    ctx.fillStyle = "red"
+    ctx.fillRect(canvas.width / 2, canvas.height / 2, 200, 200)
+
+
+    document.onkeypress = (e: KeyboardEvent) => {
+      println(e.charCode)
+    }
   }
 }
