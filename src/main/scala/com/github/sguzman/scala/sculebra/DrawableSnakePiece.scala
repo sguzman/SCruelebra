0f5f7087c70a51c6315525082264c0d230f49a89
Salvador Guzman <guzmansalv@gmail.com>
2017-12-27 20:11:36 -0800

    use circles instead

--- src/main/scala/com/github/sguzman/scala/sculebra/DrawableSnakePiece.scala
+++ src/main/scala/com/github/sguzman/scala/sculebra/DrawableSnakePiece.scala
@@ -4,8 +4,13 @@ import org.scalajs.dom
 import org.scalajs.dom.html.Canvas
 
 trait DrawableSnakePiece extends Drawable {
+  var color = "red"
+
   override def draw(implicit ctx: dom.CanvasRenderingContext2D, canvas: Canvas): Unit = {
-    ctx.fillStyle = "red"
-    ctx.fillRect(this.x, this.y, this.radius, this.radius)
+    ctx.beginPath
+    ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI)
+    ctx.stroke()
+    ctx.fillStyle = this.color
+    ctx.fill()
   }
 }
