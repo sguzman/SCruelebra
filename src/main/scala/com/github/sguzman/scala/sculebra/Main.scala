3afdada3fa2dcbf79d0b4cc5a7b65d99b30065e1
Salvador Guzman <guzmansalv@gmail.com>
2018-01-01 17:25:54 -0800

    simple moving ball

--- src/main/scala/com/github/sguzman/scala/sculebra/Main.scala
+++ src/main/scala/com/github/sguzman/scala/sculebra/Main.scala
@@ -9,6 +9,7 @@ import rxscalajs.Observable
 object Main {
   final case class Circle(x: Int, y: Int, radius: Int = 10) {
     def >:<(that: Circle): Boolean = math.sqrt(math.pow(this.x - that.x, 2) + math.pow(this.y - that.y, 2)) < this.radius
+    def +(tup: List[Int]): Circle = Circle(this.x + tup.head, this.y + tup.last, this.radius)
   }
 
   def main(args: Array[String]): Unit = {
@@ -19,19 +20,19 @@ object Main {
     val ctx: CanvasRenderingContext2D = canvas.getContext("2d")
       .asInstanceOf[dom.CanvasRenderingContext2D]
 
-    val waitTimeBetweenFrames = 125
+    val waitTimeBetweenFrames = 100
 
     val input$ = Observable.fromEvent(document.body, "keypress")
       .map(_.asInstanceOf[KeyboardEvent])
       .map(_.charCode)
 
-    var direction = (-1, 0)
+    var direction = List(-1, 0)
     var snaek = List(Circle(canvas.width / 2, canvas.height / 2))
 
-    val left$ = input$.filter(_ == 97).mapTo(-1, 0)
-    val right$ = input$.filter(_ == 100).mapTo(1, 0)
-    val up$ = input$.filter(_ == 119).mapTo(0, -1)
-    val down$ = input$.filter(_ == 115).mapTo(0, 1)
+    val left$ = input$.filter(_ == 97).mapTo(List(-1, 0))
+    val right$ = input$.filter(_ == 100).mapTo(List(1, 0))
+    val up$ = input$.filter(_ == 119).mapTo(List(0, -1))
+    val down$ = input$.filter(_ == 115).mapTo(List(0, 1))
 
     val inputs$ = left$.merge(right$).merge(up$).merge(down$).distinctUntilChanged
     inputs$.subscribe(s => direction = s)
@@ -39,6 +40,10 @@ object Main {
     val gameLoop$ = Observable.interval(waitTimeBetweenFrames)
 
     gameLoop$.subscribe(s => {
+      ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+      snaek = List(snaek.head + direction.map(_ * (snaek.head.radius * 2)))
+
       ctx.beginPath
       ctx.arc(snaek.head.x, snaek.head.y, snaek.head.radius, 0, 2 * Math.PI)
       ctx.fillStyle = "red"
