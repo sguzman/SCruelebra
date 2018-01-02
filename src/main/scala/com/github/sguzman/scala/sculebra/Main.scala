ed9b2f80bac4c1f2eb2df55fa1fdd7653b2625f2
Salvador Guzman <guzmansalv@gmail.com>
2018-01-01 21:04:09 -0800

    working wrapping

--- src/main/scala/com/github/sguzman/scala/sculebra/Main.scala
+++ src/main/scala/com/github/sguzman/scala/sculebra/Main.scala
@@ -7,7 +7,7 @@ import org.scalajs.dom.{CanvasRenderingContext2D, document}
 import rxscalajs.Observable
 
 object Main {
-  final case class Circle(x: Int, y: Int, radius: Int = 10) {
+  final case class Circle(var x: Int, var y: Int, radius: Int = 10) {
     def >:<(that: Circle): Boolean = math.sqrt(math.pow(this.x - that.x, 2) + math.pow(this.y - that.y, 2)) < this.radius
     def +(tup: List[Int]): Circle = Circle(this.x + tup.head, this.y + tup.last, this.radius)
   }
@@ -20,14 +20,14 @@ object Main {
     val ctx: CanvasRenderingContext2D = canvas.getContext("2d")
       .asInstanceOf[dom.CanvasRenderingContext2D]
 
-    val waitTimeBetweenFrames = 100
+    val waitTimeBetweenFrames = 250
 
     val input$ = Observable.fromEvent(document.body, "keypress")
       .map(_.asInstanceOf[KeyboardEvent])
       .map(_.charCode)
 
     var direction = List(-1, 0)
-    var snaek = List(Circle(canvas.width / 2, canvas.height / 2))
+    var snaek = Circle(canvas.width / 2, canvas.height / 2)
 
     val left$ = input$.filter(_ == 97).mapTo(List(-1, 0))
     val right$ = input$.filter(_ == 100).mapTo(List(1, 0))
@@ -39,13 +39,18 @@ object Main {
 
     val gameLoop$ = Observable.interval(waitTimeBetweenFrames)
 
+    gameLoop$.filter(_ => snaek.x > canvas.width).subscribe(_ => snaek.x = 0)
+    gameLoop$.filter(_ => snaek.x < 0).subscribe(_ => snaek.x = canvas.width)
+    gameLoop$.filter(_ => snaek.y > canvas.height).subscribe(_ => snaek.y = 0)
+    gameLoop$.filter(_ => snaek.y < 0).subscribe(_ => snaek.y = canvas.height)
+
     gameLoop$.subscribe(s => {
       ctx.clearRect(0, 0, canvas.width, canvas.height)
 
-      snaek = List(snaek.head + direction.map(_ * (snaek.head.radius * 2)))
+      snaek = snaek + direction.map(_ * (snaek.radius * 2))
 
       ctx.beginPath
-      ctx.arc(snaek.head.x, snaek.head.y, snaek.head.radius, 0, 2 * Math.PI)
+      ctx.arc(snaek.x, snaek.y, snaek.radius, 0, 2 * Math.PI)
       ctx.fillStyle = "red"
       ctx.fill()
     })
